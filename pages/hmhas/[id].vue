b387c0f9529c9559036bf33a4e13e19b07f41eb7
Reza Sapitra <113533664+reezyee@users.noreply.github.com>
2024-06-22 20:58:20 +0700

    add song

---
+++ pages/hmhas/[id].vue
@@ -0,0 +1,70 @@
+<template>
+    <div class="songs pt-16 pb-[50px]">
+        <NuxtLink to="/hmhas/">
+            <button type="button"
+                class=" ms-10 px-5 py-2 text-sm text-gray-700 transition-colors duration-200 border rounded-lg gap-x-2 dark:hover:bg-gray-800 dark:bg-gray-900 hover:bg-gray-100 dark:text-gray-200 dark:border-gray-700">
+                <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                    stroke-width="1.5" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
+                </svg>
+                <!-- <span></span> -->
+            </button>
+        </NuxtLink>
+        <div
+            class="content flex lg:flex-row md:flex-row sm:flex-col flex-col items-center justify-center lg:gap-20 md:gap-18 sm:gap-5 gap-5 content-center pb-14 px-10">
+            <div class="flex flex-col gap-1">
+                <img :src="songs?.cover" :alt="songs?.judul" class="w-96 shadow-md shadow-black rounded-xl">
+                <p class="text-3xl">{{ songs?.judul }}</p>
+                <p class="text-gray-200/40 text-x">{{ songs?.penyanyi }}</p>
+                <audio id="song" class="block w-full max-w-md mx-auto" controls>
+                    <source :src="songs?.lagu" type="audio/mp3">
+                </audio>
+            </div>
+            <!-- <audio :src="songs?.lagu" class="bg-white"></audio> -->
+            <div class="lyrics">
+                <div
+                    class="card lg:w-[600px] lg:h-[520px] md:w-[400px] md:h-[520px] sm:h-[300px] h-[400px] bg-base-100 shadow-xl">
+                    <div class="card-body overflow-inside overflow-y-auto">
+                        <div class="card-actions justify-end">
+                            <button class="btn btn-square btn-sm">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                                    stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                        d="M6 18L18 6M6 6l12 12" />
+                                </svg>
+                            </button>
+                        </div>
+                        <pre class="text-center text-wrap">{{ songs?.lirik }}</pre>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+// import { onMounted } from 'vue';
+
+const supabase = useSupabaseClient()
+
+const route = useRoute()
+const songs = ref()
+
+async function getSongById() {
+    const { data, error } = await supabase.from('songs')
+        .select(`*, albums(*)`)
+        .eq('id', route.params.id)
+        .maybeSingle()
+    if (data) songs.value = data
+}
+
+onMounted(() => {
+    getSongById()
+})
+</script>
+
+<style scoped>
+.songs {
+    background: linear-gradient(360deg, #02060b, #1e3e5c);
+}
+</style>
--- pages/hmhas/index.vue
+++ pages/hmhas/index.vue
@@ -1,84 +1,60 @@
 <template>
-    <div class="bg-[#303030] text-gray-300 min-h-screen p-10 pt-24">
-
-        <!-- header -->
-        <div class="flex">
-            <img class="mr-6 w-96" :src="album?.cover" >
-            <div class="flex flex-col justify-center">
-                <!-- content -->
-                <h4 class="mt-0 mb-2 uppercase text-gray-500 tracking-widest text-xs">Playlist</h4>
-                <h1 class="mt-0 mb-2 text-white text-4xl text-bold">{{album?.judul}}</h1>
-
-                <p class="mb-2 text-sm text-semibold"> {{album?.penyanyi}}</p>
-                <p class="text-gray-600 text-sm">Created by <a>Reezyee</a> - 50 songs, 3 hr 2 min</p>
-            </div>
-        </div>
-
-        <!-- action buttons -->
-        <div class="mt-6 flex justify-between">
+    <div class="bg-[#303030] pt-16">
+        <NuxtLink to="/">
+                <button type="button"
+                    class="ms-10 w-1/2 px-5 py-2 text-sm text-gray-700 transition-colors duration-200 bg-white border rounded-lg gap-x-2 sm:w-auto dark:hover:bg-gray-800 dark:bg-gray-900 hover:bg-gray-100 dark:text-gray-200 dark:border-gray-700">
+                <svg class="w-5 h-5 rtl:rotate-180" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                    stroke-width="1.5" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
+                </svg>
+            </button>
+                <!-- <span></span> -->
+            </NuxtLink>
+        <div class="text-gray-300 min-h-screen px-10 py-5">
+            <!-- header -->
             <div class="flex">
-                <button class="mr-2 bg-green-500 text-green-100 block py-2 px-8 rounded-full">Play</button>
-                <button class="mr-2 border border-white block p-2 rounded-full"><img
-                        src="https://image.flaticon.com/icons/svg/2485/2485986.svg" height="25" width="25"></button>
-                <button class="mr-2 border border-white block p-2 rounded-full">...</button>
-            </div>
-            <div class="text-gray-600 text-sm tracking-widest text-right">
-                <h5 class="mb-1">Followers</h5>
-                <p>5,055</p>
-            </div>
-        </div>
+                <img class="mr-6 w-96"
+                    src="https://yfpzsnvyrqerpascezct.supabase.co/storage/v1/object/public/cover/hmhas.jpg">
+                <div class="flex flex-col justify-center">
+                    <!-- content -->
+                    <h4 class="mt-0 mb-2 uppercase text-gray-200/50 tracking-widest text-xl">Albums</h4>
+                    <h1 class="mt-0 mb-2 text-white text-4xl text-bold">HIT ME HARD AND SOFT</h1>
 
-        <!-- song list   -->
-        <div class="mt-10">
-            <!-- song list header -->
-            <div class="flex text-gray-600">
-                <div class="p-2 w-8 flex-shrink-0"></div>
-                <div class="p-2 w-8 flex-shrink-0"></div>
-                <div class="p-2 w-full">Title</div>
-                <div class="p-2 w-full">Artist</div>
-                <div class="p-2 w-full">Album</div>
-                <div class="p-2 w-12 flex-shrink-0 text-right">⏱</div>
+                    <p class="mb-2 text-sm text-semibold">Billie Eilish, Finneas</p>
+                    <p class="text-gray-200/50 text-sm">Created by <a>Reezyee</a> - 50 songs, 3 hr 2 min</p>
+                </div>
             </div>
 
-            <div class="flex border-b border-gray-800 hover:bg-gray-800">
-                <div class="p-3 w-8 flex-shrink-0">▶️</div>
-                <div class="p-3 w-8 flex-shrink-0">❤️</div>
-                <div class="p-3 w-full">My Song Here</div>
-                <div class="p-3 w-full">Eminem</div>
-                <div class="p-3 w-full">Spotify</div>
-                <div class="p-3 w-12 flex-shrink-0 text-right">5:35</div>
+            <!-- action buttons -->
+            <div class="mt-6 flex justify-between">
+                <div class="flex">
+                    <button class="mr-2 bg-green-500 text-green-100 block py-2 px-8 rounded-full">Play</button>
+                </div>
             </div>
-            <div class="flex border-b border-gray-800 hover:bg-gray-800">
-                <div class="p-3 w-8 flex-shrink-0">▶️</div>
-                <div class="p-3 w-8 flex-shrink-0">❤️</div>
-                <div class="p-3 w-full">My Song Here</div>
-                <div class="p-3 w-full">Eminem</div>
-                <div class="p-3 w-full">Spotify</div>
-                <div class="p-3 w-12 flex-shrink-0 text-right">5:35</div>
-            </div>
-            <div class="flex border-b border-gray-800 hover:bg-gray-800">
-                <div class="p-3 w-8 flex-shrink-0">▶️</div>
-                <div class="p-3 w-8 flex-shrink-0">❤️</div>
-                <div class="p-3 w-full">My Song Here</div>
-                <div class="p-3 w-full">Eminem</div>
-                <div class="p-3 w-full">Spotify</div>
-                <div class="p-3 w-12 flex-shrink-0 text-right">5:35</div>
-            </div>
-            <div class="flex border-b border-gray-800 hover:bg-gray-800">
-                <div class="p-3 w-8 flex-shrink-0">▶️</div>
-                <div class="p-3 w-8 flex-shrink-0">❤️</div>
-                <div class="p-3 w-full">My Song Here</div>
-                <div class="p-3 w-full">Eminem</div>
-                <div class="p-3 w-full">Spotify</div>
-                <div class="p-3 w-12 flex-shrink-0 text-right">5:35</div>
-            </div>
-            <div class="flex border-b border-gray-800 hover:bg-gray-800">
-                <div class="p-3 w-8 flex-shrink-0">▶️</div>
-                <div class="p-3 w-8 flex-shrink-0">❤️</div>
-                <div class="p-3 w-full">My Song Here</div>
-                <div class="p-3 w-full">Eminem</div>
-                <div class="p-3 w-full">Spotify</div>
-                <div class="p-3 w-12 flex-shrink-0 text-right">5:35</div>
+
+            <!-- song list   -->
+            <div class="mt-10">
+                <!-- song list header -->
+                <div class="flex text-gray-200/50">
+                    <!-- <div class="p-2 w-8 flex-shrink-0"></div>
+                <div class="p-2 w-8 flex-shrink-0"></div> -->
+                    <div class="p-2 w-full">Title</div>
+                    <div class="p-2 w-full">Artist</div>
+                    <div class="p-2 w-full">Album</div>
+                    <!-- <div class="p-2 w-12 flex-shrink-0 text-right">⏱</div> -->
+                </div>
+                <div v-for="(song, i) in songs" :key="i" class="">
+                    <NuxtLink :to="`/hmhas/${song.id}`" class="no-underline">
+                        <div class="flex border-b border-gray-800 hover:bg-gray-800">
+                            <!-- <div class="p-3 w-8 flex-shrink-0">▶️</div>
+                        <div class="p-3 w-8 flex-shrink-0">❤️</div> -->
+                            <div class="p-3 w-full">{{ song.judul }}</div>
+                            <div class="p-3 w-full">{{ song.penyanyi }}</div>
+                            <div class="p-3 w-full">{{ song.albums?.judul }}</div>
+                            <!-- <div class="p-3 w-12 flex-shrink-0 text-right">{{ song.lagu }}</div> -->
+                        </div>
+                    </NuxtLink>
+                </div>
             </div>
         </div>
     </div>
@@ -87,13 +63,15 @@
 <script setup>
 const supabase = useSupabaseClient()
 
-const albums = ref([])
+const songs = ref([])
 
 async function getSongs() {
-    const {data, error} = await supabase.from('album')
-    .select(`*, album(*)`)
-    if (data) albums.value = data 
+    const { data, error } = await supabase.from('songs')
+        .select(`*, albums(*)`)
+        .order('id')
+    if (data) songs.value = data
 }
+
 
 onMounted(() => {
     getSongs()
